ad6d44c49ad4444c2e30c6c791e92ca203d32ae2
AOthman97 <44799617+AOthman97@users.noreply.github.com>
2022-03-15 05:42:54 +0200

    Add project files.

---
+++ Data/SmartSchoolsDBContext.cs
@@ -0,0 +1,35 @@
+﻿namespace SmartSchools.Data
+{
+    public class SmartSchoolsDBContext
+    {
+        public SmartSchoolsDBContext()
+        {
+
+        }
+
+        //public SmartSchoolsDBContext(dbcontextoptions options) : base(options)
+        //{
+
+        //}
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        IConfigurationRoot configuration = new ConfigurationBuilder()
+        //           .SetBasePath(Directory.GetCurrentDirectory())
+        //           .AddJsonFile("appsettings.json")
+        //           .Build();
+        //        var connectionString = configuration.GetConnectionString("DefaultConnectionString");
+        //        optionsBuilder.UseSqlServer(connectionString);
+        //    }
+        //}
+
+        //// Each individual db set represents a table in our DB
+        //public DbSet<Actor> Actors { get; set; }
+        //public DbSet<Movie> Movies { get; set; }
+        //public DbSet<Producer> Producers { get; set; }
+        //public DbSet<Cinema> Cinemas { get; set; }
+        //public DbSet<ActorsMovies> ActorsMovies { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.FileProviders;
+
+namespace SmartSchools
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // The path that would contain the vocabulary data
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            // Add the localization for the app
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+            // Define which languages to support
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+
+            });
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Shared/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(@"D:\Repos\MoviesStore\wwwroot", "Images")),
+                RequestPath = "/Images"
+            });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action/Index}/{id?}"
+                    );
+            });
+        }
+    }
+}
